5471882b644ae4a84262b8264907f2a10bdbb3e5
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-06-10 15:06:44 -0700

    Smart pointer

--- examples/bunny/src/bunny.cpp
+++ examples/bunny/src/bunny.cpp
@@ -10,23 +10,25 @@
 int main(int argc, char** argv) {
   // Initialization of display
 #ifndef BENCHMARK_MODE
-  kiwigl::Display display("KiwiGL Stanford Bunny Demo", false, kiwigl::Vector3D(0, 0, -0.25),
-                          kiwigl::Vector3D(M_PI, M_PI, 0));
+  // Initialization of display
+  kiwigl::Display* display =
+      new kiwigl::Display("KiwiGL Stanford Bunny Demo", false, kiwigl::Vector3D(0, 0, -0.25),
+                          kiwigl::Vector3D(M_PI, M_PI, 0), kiwigl::Vector3D(0.005, 0.005, 0.005));
 #else
   kiwigl::Display display(10000);
 #endif
   // Load the Stanford bunny mesh
-  display.loadMesh("./assets/bunny.mesh");
+  display->loadMesh("./assets/bunny.mesh");
 
   // Main graphics loop
   // Loop until window close button is pressed
-  while (!display.shouldClose()) {
+  while (!display->shouldClose()) {
 #ifdef BENCHMARK_MODE
-    display.update();
+    display->update();
 #else
-    display.processInput();
-    display.update();
-    display.render();
+    display->processInput();
+    display->update();
+    display->render();
 #endif
   }
   return 0;
